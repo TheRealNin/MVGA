6b02dab4a0599b0dcc0732e195f7afbc624c23ee
Tik <tik@lag.party>
2018-12-21 13:56:51 -0300

    Change alien crag, shift, shade, whip and clogs to 90% of original HP

---
+++ output/lua/MVGA/StructureHP/Post/BalanceHealth.lua
@@ -0,0 +1,13 @@
+kCragHealth = 540 -- was 600
+kMatureCragHealth = 630 -- was 700
+
+kShiftHealth = 675 -- was 750
+kMatureShiftHealth = 990 -- was 1100
+
+kShadeHealth = 675 -- was 750
+kMatureShadeHealth = 1350 -- was 1500
+
+kWhipHealth = 585 -- was 650
+kMatureWhipHealth = 648 -- was 720
+
+kClogHealth = 225 -- was 250
