4b9e3e7eb65b15b8fa4c69b61b9fbb17e71e704e
Tik <tik@lag.party>
2018-12-21 14:29:21 -0300

    Resource costs - Issue #9 and #10

---
+++ output/lua/MVGA/Costs/Post/Balance.lua
@@ -0,0 +1,24 @@
+-- ALIENS
+kAlienInitialIndivRes = 16 -- was 12
+
+kGorgeTunnelCost = 5 --was 3
+
+kLerkCost = 25 -- was 21
+kFadeCost = 41 -- was 37
+kOnosCost = 66 -- was 62
+
+
+-- MARINES
+kMarineInitialIndivRes = 17 -- was 15
+
+kShotgunCost = 22 -- was 20
+kShotgunDropCost = 22 -- was 20
+
+kHeavyMachineGunDropCost = 22 -- was 20
+kHeavyMachineGunTechResearchCost = 22 -- was 20
+
+kFlamethrowerCost = 18 -- was 20
+kFlamethrowerDropCost = 18 -- was 20
+
+kGrenadeLauncherCost = 18 -- was 20
+kGrenadeLauncherDropCost = 18 -- was 20
