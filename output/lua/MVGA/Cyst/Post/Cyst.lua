24993baf14845cf55464ad66680eef73a7ecf631
Nin <therealnin@users.noreply.github.com>
2018-12-29 15:46:02 -0500

    Effectively remove cyst ability to lose maturation when disconnected, closes #24

--- output/lua/MVGA/Cyst/Post/Cyst.lua
+++ output/lua/MVGA/Cyst/Post/Cyst.lua
@@ -1,4 +1,7 @@
 
+-- we set this to a very high number since cysts now lose health instead of just becoming immature
+Cyst.kMaturityLossTime = 9001 -- was 15
+
 if Server then
   
 	local oldOnUpdate = Cyst.OnUpdate
