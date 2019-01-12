e045ada30568f7464d199f213c3532cc45a5ee3d
Bleuitup <46002006+Bleuitup@users.noreply.github.com>
2019-01-12 02:03:21 -0300

    Update Balance.lua

--- output/lua/MVGA/Exo/Post/Balance.lua
+++ output/lua/MVGA/Exo/Post/Balance.lua
@@ -7,5 +7,5 @@ kMinigunDamage = 8 -- was 10
 kMinigunDamageType = kDamageType.HeavyToPlayers -- was kDamageType.Normal
 
 kRailgunDamage = 14 -- was 10
-kRailgunChargeDamage = 200 -- was 140
+kRailgunChargeDamage = 189 -- was 140
 kRailgunDamageType = kDamageType.Railgun -- was kDamageType.Structural
